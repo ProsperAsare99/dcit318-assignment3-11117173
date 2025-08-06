2324a1ae4171fffe0d0179dd0a0ebddef4ac9e05
PROSPER SHAIBU ASARE <psasare001@st.ug.edu.gh>
2025-08-06 15:10:01 +0000

    Transaction class defined properly

--- FinanceManagementSystem/obj/Debug/net10.0/FinanceManagementSystem.AssemblyInfo.cs
+++ FinanceManagementSystem/obj/Debug/net10.0/FinanceManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3d0a18c81f33b72ff054ba767c6c80d664a10cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca6ef185dcc4feca4fd4f2a358795afe8f9b09fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
